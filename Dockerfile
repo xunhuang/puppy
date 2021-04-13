29e027262f7a431b3d0081b49649ebb636d271ac
Wilson Huang (Growth) <xhuang@fb.com>
2021-04-12 20:49:05 -0700

    add tor options to crawler

--- Dockerfile
+++ Dockerfile
@@ -6,11 +6,12 @@ RUN apt-get update && \
     libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 \
     libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 \
     libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 \
-    ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
+    ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget tor
 
 # Start the app
 WORKDIR /usr/src/app
 COPY package*.json ./
+COPY torrc /etc/tor/torrc
 ENV NODE_ENV=production
 RUN npm install --production
 COPY . .
--- screenshot.js
+++ screenshot.js
@@ -1,28 +1,46 @@
 const puppeteer = require('puppeteer');
 
+var useTor = false;
+
+const ports = [
+  9050,
+  9052,
+  9053,
+  9054
+];
+
+var nextPortIndex = 0;
+
 exports.screenshot = async (req, res) => {
-    const url = req.query.url;
+  const url = req.query.url;
 
-    if (!url) {
-      return res.send('Please provide URL as GET parameter, for example: <a href="?url=https://example.com">?url=https://example.com</a>');
-    }
+  if (!url) {
+    return res.send('Please provide URL as GET parameter, for example: <a href="?url=https://example.com">?url=https://example.com</a>');
+  }
 
-    const browser = await puppeteer.launch({
-      args: ['--no-sandbox']
-    });
-    const page = await browser.newPage();
-    await page.goto(url);
-    const imageBuffer = await page.screenshot();
-    const html = await page.content();
-    browser.close();
-    // res.set('Content-Type', 'image/png');
-    // res.send(imageBuffer);
-    res.set('Content-Type', 'text/html');
-    let json = {
-        image: imageBuffer.toString('base64'),
-        text: html,
-    }
+  let args = ['--no-sandbox'];
+  if (useTor) {
+    args.push(
+      `--proxy-server=socks5://127.0.0.1:${ports[nextPortIndex]}`
+    );
+    nextPortIndex = (nextPortIndex + 1) % ports.length;
+  }
 
-    res.send(JSON.stringify(json, null, 2));
-  };
+  const browser = await puppeteer.launch({
+    args: args
+  });
+  const page = await browser.newPage();
+  await page.goto(url);
+  const imageBuffer = await page.screenshot();
+  const html = await page.content();
+  browser.close();
+  // res.set('Content-Type', 'image/png');
+  // res.send(imageBuffer);
+  res.set('Content-Type', 'text/html');
+  let json = {
+    image: imageBuffer.toString('base64'),
+    text: html,
+  }
 
+  res.send(JSON.stringify(json, null, 2));
+};
